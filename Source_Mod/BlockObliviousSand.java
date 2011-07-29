a0e9bf9f23d802c3e99e183598a8efd5b6513776
unknown <salwa@.(none)>
2011-07-29 23:05:45 +0200

    Ajout de BlockObliviousSand
    Pour tester

---
+++ Source_Mod/BlockObliviousSand.java
@@ -0,0 +1,18 @@
+package net.minecraft.src;
+
+import java.util.Random;
+
+public class BlockObliviousSand extends Block
+{
+	
+    public BlockObliviousSand(int i, int j)
+    {
+        super(i, j, Material.sand);
+    }
+    
+    public int idDropped(int i, Random random)
+    {
+        return mod_Aelyth.ObliviousSand.blockID;
+    }
+    
+}
